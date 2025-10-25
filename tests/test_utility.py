a101cbf54e9d880580c83e11a727b5e9aa4e5247
Jeremy Calloway <xntricweb@gmail.com>
2025-10-25 14:16:28 -0400

    added features to utility

--- tests/test_utility.py
+++ tests/test_utility.py
@@ -1,7 +1,5 @@
 from typing import List, Literal
-from xntricweb.xapi.utility import (
-    get_origin_args,
-)
+from xntricweb.xapi.utility import get_origin_args, is_any, coalesce
 
 
 def test_origin_args():
@@ -17,3 +15,36 @@ def test_origin_args():
     assert get_origin_args(X) == (X, ())
     assert get_origin_args(X[str]) == (X, (str,))
     assert get_origin_args(Literal["abc", "123"]) == (Literal, ("abc", "123"))
+
+
+def test_is_any() -> None:
+    assert is_any(type(""), [str])
+    assert is_any(True, [True])
+    assert is_any(False, [False])
+    assert not is_any(None, [False])
+    assert not is_any(False, [None])
+    assert not is_any(True, [False, None])
+    assert is_any(True, [False, True])
+
+
+def test_coalesce() -> None:
+    like_none = object()
+
+    assert coalesce(1, None) == 1
+    assert coalesce(None, None, 1) == 1
+    assert coalesce(False, False, 1) == 1
+    assert coalesce("", "", 2) == 2
+    assert coalesce(True, False, 1) is True
+    assert coalesce("", False, None) is None
+    assert coalesce(None, like_none, None, 4) is like_none
+    assert (
+        coalesce(None, like_none, is_not=[like_none], check_falsey=False)
+        is None
+    )
+    assert coalesce(None, like_none, False, 4, also_is_not=[like_none]) == 4
+    assert coalesce(None, like_none, None, 4, is_not=[None, like_none]) == 4
+    assert coalesce("", like_none, None, 4, is_not=[None, like_none]) == 4
+    assert "" == coalesce(None, "", None, is_not=[None], check_falsey=False)
+    assert False is coalesce(
+        None, False, None, is_not=[None], check_falsey=False
+    )
--- xntricweb/xapi/utility.py
+++ xntricweb/xapi/utility.py
@@ -1,4 +1,4 @@
-from typing import get_args, get_origin, Any
+from typing import Optional, get_args, get_origin, Any
 
 from .const import NOT_SPECIFIED, AnyType
 
@@ -14,9 +14,24 @@ def get_origin_args(_type: AnyType) -> tuple[AnyType, tuple[AnyType, ...]]:
     return _type, args
 
 
-def coalesce(*args: Any) -> Any:
+def is_any(value: Any, classes: list[Any]):
+    return any(value is _class for _class in classes)
+
+
+def coalesce(
+    *args: Any,
+    is_not: Optional[list[Any]] = None,
+    also_is_not: Optional[list[Any]] = None,
+    check_falsey: bool = True,
+) -> Any:
+    if not is_not:
+        is_not = [None, False]
+
+    if also_is_not:
+        is_not.extend(also_is_not)
+
     for arg in args:
-        if arg is not None and arg is not NOT_SPECIFIED:
+        if not is_any(arg, is_not) and (not check_falsey or arg):
             return arg
 
     return args[-1]
